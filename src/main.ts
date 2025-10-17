b7de3b9ddac98282271a35c31e06da64010563f8
Pl0the <eclap9145@gmail.com>
2025-10-17 01:49:17 +0000

    Added logic to allow for drawing on the canvas, added button to clear the canvas, changed overall formatting (Step 2).

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,47 @@
-import "./style.css";
-
-const h1 = document.createElement("h1");
-h1.textContent = "Sticker Sketchpad";
-document.body.append(h1);
+const title = document.createElement("h1");
+title.textContent = "My Canvas App";
+document.body.append(title);
 
 const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
-canvas.style.width = "256px";
-canvas.style.height = "256px";
 canvas.id = "sketch_canvas";
 document.body.append(canvas);
 
+document.body.style.textAlign = "center";
+
 const ctx = canvas.getContext("2d");
-if (ctx) {
-  ctx.fillStyle = "green";
-  ctx.fillRect(10, 10, 150, 100);
-}
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active && ctx) {
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.moveTo(cursor.x, cursor.y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerText = "Clear";
+document.body.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+});
